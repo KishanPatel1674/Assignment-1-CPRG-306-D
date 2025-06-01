11fd5b78a9aebe982210bc158f894c5920327b8e
KishanPatel1674 <kishanpatel1935@gmail.com>
2025-05-31 23:28:02 -0600

    Update script.js with weight

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showPage(pageId) {
+  var pages = document.querySelectorAll('.page');
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].classList.remove('active');
+  }
+  document.getElementById(pageId).classList.add('active');
+}
 
+function handleConvert(event, type) {
+  event.preventDefault();
+
+  var fromUnit, toUnit, inputValue, resultId;
+
+  switch (type) {
+    case 'weight':
+      fromUnit = document.getElementById('fromWeight').value;
+      toUnit = document.getElementById('toWeight').value;
+      inputValue = document.getElementById('inputWeight').value;
+      resultId = 'resultWeight';
+      break;
